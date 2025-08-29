ceb0022477dfededf4b1ef279576789cced9f08b
Leo Nickl <leo@leonickl.de>
2025-08-29 23:07:46 +0200

    refactor

--- main.py
+++ main.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python3
+
 import subprocess
 import os
 import tempfile
-strWNtempdir=tempfile.gettempdir()
-strWNtmpfile=strWNtempdir+'/temp.jpg'
-strWNcom = 'magick import ' + strWNtmpfile
-process = subprocess.call([strWNcom], shell=True)
-strWNcom = 'magick  ' + strWNtmpfile + ' -contrast-stretch 1% -level 29%,76% -compress jpeg -strip -quality 40% ' + strWNtmpfile
-process = subprocess.call([strWNcom], shell=True)
-strWNcom = 'tesseract ' + strWNtmpfile + ' stdout -l deu | xclip -in -selection clipboard'
-process = subprocess.call(strWNcom,shell=True)
-print(strWNtmpfile, os.path.exists(strWNtmpfile))
-if os.path.isfile(strWNtmpfile) or os.path.islink(strWNtmpfile):
-            os.remove(strWNtmpfile) 
+
+file = tempfile.gettempdir() + '/temp.jpg'
+
+subprocess.call([
+    'magick import ' + file
+], shell = True)
+
+subprocess.call([
+    'magick  ' + file + ' -contrast-stretch 1% -level 29%,76% -compress jpeg -strip -quality 40% ' + file
+], shell = True)
+
+subprocess.call([
+    'tesseract ' + file + ' stdout -l deu | xclip -in -selection clipboard'
+], shell = True)
+
+print(file, os.path.exists(file))
+
+if os.path.isfile(file) or os.path.islink(file):
+    os.remove(file)
